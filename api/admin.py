8d7e1ffc334c1ae106a0ad47c2dde877df44ca80
ghkim <gkim9213@gmail.com>
2023-01-30 11:06:01 +0900

    remove api models

--- api/admin.py
+++ api/admin.py
@@ -1,18 +1,2 @@
 from .models import *
 from django.contrib import admin
-
-@admin.register(Content)
-class ContentAdmin(admin.ModelAdmin):
-    pass
-
-@admin.register(ContentTag)
-class ContentTagAdmin(admin.ModelAdmin):
-    pass
-
-@admin.register(ContentParameter)
-class ContentParameterAdmin(admin.ModelAdmin):
-    pass
-
-@admin.register(ContentResponse)
-class ContentResponseAdmin(admin.ModelAdmin):
-    pass
